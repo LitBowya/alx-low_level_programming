903af970ba547640a4a8474beda9888972c51773
LitBowya <agbagabenjamin819@gmail.com>
2022-12-09 04:24:47 -0800

    Corrected a typo

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,11 +5,10 @@
 /**
  * main - assigns a random number to int n
  * it executes the program, and prints n
- * Return: Always 0 (Success) 
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-
 	int n;
 
 	srand(time(0));
